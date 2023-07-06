98b61d20bc1d2f1e91a4c6640b2f68f47cf4edee
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-06 11:43:04 -0500

    [+] food.component: Obtiene 'Fake Food' del service

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  foods: Food[] = [];
+
+  constructor( private foodService: FoodService ) {
+    this.foods = foodService.getAll();
+  }
 
 }
