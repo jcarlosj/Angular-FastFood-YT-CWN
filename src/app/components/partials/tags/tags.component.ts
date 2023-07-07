97915ed93fa000501ec26d4c862227c089569d3a
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-07 12:52:16 -0500

    [+] tags.component: Obtiene y despliega tags de 'Fake Tags' usando el FoodService [+] data.ts: minusculas para todos los tags

--- src/app/components/partials/tags/tags.component.ts
+++ src/app/components/partials/tags/tags.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FoodService } from 'src/app/services/food.service';
+import { Tag } from 'src/app/shared/models/Tag';
 
 @Component({
   selector: 'app-tags',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./tags.component.css']
 })
 export class TagsComponent {
+  tags?: Tag[];
 
+  /** Especificación de Acceso en TypeScript
+   * public - puede acceder a los miembros desde fuera de la clase.
+   * private - NO puede acceder a los miembros (o verlos) desde fuera de la clase
+   * protected - NO puede acceder a los miembros desde fuera de la clase, sin embargo, se puede acceder a ellos en clases heredadas
+   *
+   * NOTA: Cuando no se especifica el nivel de acceso de una dependencia inyectada en el controlador como es el caso de 'activatedRoute'
+   *       esta solo estará disponible para ese bloque de código es decir solo dentro del controlador, por esta misma razón no usaremos el this
+   *       para acceder a ella, ya que no quedaría en el ámbito global de la clase
+   *
+   * @param foodService
+   */
+
+  constructor(
+    foodService: FoodService
+  ) {
+    this.tags = foodService.getAllTags();
+  }
 }
