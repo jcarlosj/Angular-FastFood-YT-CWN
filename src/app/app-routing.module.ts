58c421738759977f19d65030134733ca7e240c42
Juan Carlos Jiménez Gutiérrez <jcjimenez29@misena.edu.co>
2023-07-11 19:23:35 -0500

    [+] food.component: Define estructura y estilos [+] app-routing.module: nueva ruta food/:id

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { FoodComponent } from './pages/food/food.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'search/:term', component: HomeComponent },
-  { path: 'tag/:tag', component: HomeComponent }
+  { path: 'tag/:tag', component: HomeComponent },
+  { path: 'food/:id', component: FoodComponent }
 ];
 
 @NgModule({
--- src/app/pages/food/food.component.ts
+++ src/app/pages/food/food.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { FoodService } from 'src/app/services/food.service';
+import { Food } from 'src/app/shared/models/Food';
 
 @Component({
   selector: 'app-food',
@@ -6,5 +9,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./food.component.css']
 })
 export class FoodComponent {
+  food!: Food;
 
+  constructor(
+    private foodService: FoodService,
+    activatedRoute: ActivatedRoute    // -> Brinda acceso a información sobre una ruta asociada a un componente que se carga.                                              //    Úselo para atravesar el árbol RouterState y extraer información de los nodos.
+  ) {
+    activatedRoute.params.subscribe( params => {
+      if( params[ 'id' ]) {
+        this.food = foodService.getFoodById( params[ 'id' ]);
+      }
+    });
+  }
 }
